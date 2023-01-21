13ed53134093f3c7a1ac8ed2a4333c8765f79a3b
KJakubowicz <kjakubowicz98@interia.pl>
2023-01-21 13:53:01 +0100

    Added model for app

---
+++ app/src/Entity/Payments.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaymentsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaymentsRepository::class)]
+class Payments
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $total = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $last_modification = null;
+
+    #[ORM\Column]
+    private ?int $created_by = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTotal(): ?string
+    {
+        return $this->total;
+    }
+
+    public function setTotal(?string $total): self
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getLastModification(): ?\DateTimeImmutable
+    {
+        return $this->last_modification;
+    }
+
+    public function setLastModification(?\DateTimeImmutable $last_modification): self
+    {
+        $this->last_modification = $last_modification;
+
+        return $this;
+    }
+
+    public function getCreatedBy(): ?int
+    {
+        return $this->created_by;
+    }
+
+    public function setCreatedBy(int $created_by): self
+    {
+        $this->created_by = $created_by;
+
+        return $this;
+    }
+}
